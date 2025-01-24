5e1e7085cf969ab30a444af143892332c49220d3
bernibauque <bernarditabauque@gmail.com>
2025-01-23 21:37:31 -0300

    clase 6

---
+++ clase6/Ejercicios/ejercicio2/ejercicio2.js
@@ -0,0 +1,8 @@
+// Mostramos la plataforma actual.
+console.log(`Estás usando el sistema operativo: ${process.platform}`);
+
+// Mostramos el directorio actual de trabajo.
+console.log(`El programa se ejecuta desde el directorio: ${process.cwd()}`);
+
+
+
