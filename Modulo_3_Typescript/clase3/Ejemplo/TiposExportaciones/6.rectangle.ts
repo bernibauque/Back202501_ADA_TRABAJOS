1039f7080075ee9a6d9e8cc680f2fd103b5f65d2
bernibauque <bernarditabauque@gmail.com>
2025-07-23 21:07:11 -0300

    clase 2 y 3

---
+++ Modulo_3_Typescript/clase3/Ejemplo/TiposExportaciones/6.rectangle.ts
@@ -0,0 +1,10 @@
+// EXPORTACION SEPARADA
+
+const calculateRectangleArea = (width:number, height:number):number => {
+    return width * height
+}
+
+const RECTANGLE_NAME = 'Rectangle'
+
+export {calculateRectangleArea, RECTANGLE_NAME}
+
---
+++ Modulo_3_Typescript/clase3/Ejemplo/TiposExportaciones/8.index.ts
@@ -0,0 +1,7 @@
+// Re- exportamos todo lo exportado en circle.ts, rectangle.ts y trinagle.ts
+// esto nos permite importar todos los modulos desde el index
+
+export * from './5.circle'
+export * from './6.rectangle'
+export * from './7.triangle'
+
