0230add8bbe85fdce3d913f184251a45859daa98
bernibauque <bernarditabauque@gmail.com>
2025-08-06 20:47:20 -0300

    clase 6

--- Modulo_3_Typescript/clase5/Ejercicios/ejercicio2.ts
+++ Modulo_3_Typescript/clase5/Ejercicios/ejercicio2.ts
@@ -1,10 +1,25 @@
-// Definir el objeto Libro
-export const libro = {
-    titulo: "El Principito",
-    autor: "Antoine de Saint-Exupéry",
-    añoPublicacion: 1943
-  };
-  
-  // Mostrar las propiedades del libro
-  console.log(libro);
+/**Ejercicio 2: Creación de Objetos y Atributos
+1.Define un objeto que represente un libro con las siguientes 
+propiedades: título, autor, y año de publicación.
+2.Crea una instancia de ese objeto y muestra sus propiedades en la consola. */
+
+//2.interfaz
+interface Libro {
+  titulo: string;
+  autor: string;
+  anioPublicacion: number;
+}
+
+//1. objeto
+const miLibro: Libro = {
+  titulo: "Cien años de soledad",
+  autor: "Gabriel García Márquez",
+  anioPublicacion: 1967
+};
+
+// Instancia
+console.log("Título:", miLibro.titulo);
+console.log("Autor:", miLibro.autor);
+console.log("Año de publicación:", miLibro.anioPublicacion);
+
   
---
+++ Modulo_3_Typescript/clase6/Ejemplos/1.clases.ts
@@ -0,0 +1,41 @@
+// HERENCIA
+
+// Clase padre / super clase / clase base
+class Animal {
+    name: string; // propiedad que almacena el nombre del animal
+
+    constructor (name:string) {
+        this.name = name
+    }
+
+    makeSound(): void {
+        console.log(`${this.name} hace un sonido`);
+    }
+}
+
+// Clase hija / subclase / clase derivada
+class Dog extends Animal {
+    raza: string // nueva propiedad para la clase dog
+
+    constructor(name: string, raza:string) {
+        super(name) // llama al constructor de la clase animal para inicializar name
+        this.raza = raza
+    }
+
+    // sobrescritura de metodo de clase padre
+    makeSound(): void {
+        console.log(`${this.name} ladra`);
+    }
+
+    juego(): void {
+        console.log(`${this.name} esta buscando la pelota`);
+    }
+}
+
+// Instanciar:
+const genericAnimal = new Animal('Animal')
+genericAnimal.makeSound()
+
+const myDog = new Dog('Blaki', 'caniche')
+myDog.makeSound()
+myDog.juego()
